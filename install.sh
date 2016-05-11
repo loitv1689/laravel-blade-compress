1c9e6f192a088c35371de12286938eabeabaff79
wangyuzju <wangyu0248@gmail.com>
2016-05-11 18:32:12 +0800

    update install script and usage manual

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 TARGET_FILE_PATH="./vendor/laravel/framework/src/Illuminate/View/Compilers/BladeCompiler.php";
+FILE_DOWNLOAD_URL="https://raw.githubusercontent.com/wangyuzju/laravel-blade-compress/master/Compilers/BladeCompiler.php"
 
-
-if [ -e $TARGET_FILE_PATH ]
+if [  -e $TARGET_FILE_PATH ]
 then
-        wget https://raw.githubusercontent.com/wangyuzju/laravel-blade-compress/master/Compilers/BladeCompiler.php
+        wget $FILE_DOWNLOAD_URL -O $TARGET_FILE_PATH
 else
         echo "can not find replace target at path '$TARGET_FILE_PATH'";
         echo "please make sure running this command under laravel project root!";
